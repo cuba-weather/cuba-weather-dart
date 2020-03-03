94744491162e634ab29e698dcc80d44571a307a4
leynier <leynier41@gmail.com>
2020-03-03 16:33:57 -0500

    Export repositories

--- lib/src/repositories/repositories.dart
+++ lib/src/repositories/repositories.dart
@@ -1,3 +1,5 @@
+export 'insmet_forecast_repository.dart';
+export 'insmet_marine_forecast_repository.dart';
 export 'insmet_source_repository.dart';
 export 'insmet_weather_repository.dart';
 export 'municipality_repository.dart';
